6d56be337622ad5bb5371bf4adf3056382f32e28
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-18 19:52:08 -0400

    refactor console.log print all retreived data entries

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,32 @@
-const request = require('request');
-const fs = require('fs');
-const readline = require('readline');
-const { type } = require('os');
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline");
+const { type } = require("os");
 
 // function to fetch siberian breed description
 const fetchBreedDescription = (breed) => {
   // request to fetch breed description from API
-  request ( "https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
-    if (error) {
-      // print error and exit if one occurred
-      console.log("Breed Not Found:", error.hostname);
-      process.exit();
-    } else {
-      // if no error, check if response is 200: OK
-      if (response.statusCode === 200) {
-        // convert the body to raw data
-        const data = JSON.parse(body);
-        // log the data
-        console.log(data[0]['description']);
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=sib",
+    (error, response, body) => {
+      if (error) {
+        // print error and exit if one occurred
+        console.log("Breed Not Found:", error.hostname);
+        process.exit();
+      } else {
+        // if no error, check if response is 200: OK
+        if (response.statusCode === 200) {
+          // convert the body to raw data
+          const data = JSON.parse(body);
+          // log the data
+          for (const obj of data) {
+            console.log(obj.description);
+          }
+        }
       }
     }
-
-  });
+  );
 };
 
 fetchBreedDescription();
 //const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + searchTerm;
-
