65f5480aded3b2328b8ca980f09db8bd0e8fbe4b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-18 20:14:35 -0400

    enable multi breed input implement breed not found flag.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,33 @@
 const request = require("request");
-const fs = require("fs");
-const readline = require("readline");
-const { type } = require("os");
 
+const args = process.argv.slice(2);
 // function to fetch siberian breed description
 const fetchBreedDescription = (breed) => {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
   // request to fetch breed description from API
-  request(
-    "https://api.thecatapi.com/v1/breeds/search?q=sib",
-    (error, response, body) => {
-      if (error) {
-        // print error and exit if one occurred
-        console.log("Breed Not Found:", error.hostname);
-        process.exit();
-      } else {
-        // if no error, check if response is 200: OK
-        if (response.statusCode === 200) {
-          // convert the body to raw data
-          const data = JSON.parse(body);
-          // log the data
-          for (const obj of data) {
-            console.log(obj.description);
-          }
+  request(URL, (error, response, body) => {
+    if (error) {
+      // print error and exit if one occurred
+      console.log("URL not found:", error.hostname);
+      process.exit();
+    } else {
+      // if no error, check if response is 200: OK
+      if (response.statusCode === 200) {
+        // convert the body to raw data
+        const data = JSON.parse(body);
+        // log the data
+        if (data.length === 0) {
+          console.log("Breed Not Found");
+        }
+        for (const obj of data) {
+          console.log(obj.description);
         }
       }
     }
-  );
+  });
 };
 
-fetchBreedDescription();
-//const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + searchTerm;
+// call the function
+for (const arg of args) {
+  fetchBreedDescription(arg);
+}
